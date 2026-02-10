4a0d24d58d120c1337f8636a652821219429558f
Yaman Aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2026-02-10 15:54:53 -0500

    Arm positions

--- SnadBox/Obtaining_Hand_Position.py
+++ SnadBox/Obtaining_Hand_Position.py
@@ -1,3 +1,5 @@
+# Right Arm
+
 from reachy2_sdk import ReachySDK
 import time
 
@@ -26,3 +28,34 @@ try:
 
 except KeyboardInterrupt:
     print("\n--- Monitoring Stopped by User ---")
+
+
+# Left Arm 
+# from reachy2_sdk import ReachySDK
+# import time
+
+# reachy = ReachySDK(host='192.168.50.241') 
+
+# reachy.l_arm.turn_off()
+
+# print("--- Reachy is now in Compliant Mode ---")
+# print("Move the arm to the positions you want to record.")
+# print("The code will print the joint angles in a format you can copy-paste.")
+# print("Press Ctrl+C to stop.\n")
+
+# try:
+#     while True:
+#         s_pitch = reachy.l_arm.shoulder.pitch.present_position
+#         s_roll  = reachy.l_arm.shoulder.roll.present_position
+#         e_yaw   = reachy.l_arm.elbow.yaw.present_position
+#         e_pitch = reachy.l_arm.elbow.pitch.present_position
+
+#         print(f"{{ 'l_arm.shoulder.pitch': {s_pitch:.1f}, "
+#               f"'l_arm.shoulder.roll': {s_roll:.1f}, "
+#               f"'l_arm.elbow.yaw': {e_yaw:.1f}, "
+#               f"'l_arm.elbow.pitch': {e_pitch:.1f} }}")
+
+#         time.sleep(0.5)
+
+# except KeyboardInterrupt:
+#     print("\n--- Monitoring Stopped by User ---")
